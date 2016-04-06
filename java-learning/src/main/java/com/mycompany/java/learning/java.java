a5c950ad271b0cfc69d7862605976461684e4f80
gpiskunov <gpiskunov@luxoft.com>
2016-04-06 10:27:05 +0300

    Перенос файлов.

---
+++ java-learning/src/main/java/com/mycompany/java/learning/java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.java.learning;
+
+import java.lang.reflect.Array;
+
+/**
+ *
+ * @author gpiskunov
+ */
+public class java {
+    int arrTest[] = {1, 2, 3, 4, 5}; 
+    long []arrTest2 = {2, 3, 4, 5};
+    char []arrCh = new char[5];
+    
+  
+    
+}
