db7c253c286a07d058e3d59ca020ef64948435fd
gpiskunov <gpiskunov@luxoft.com>
2016-05-27 17:54:15 +0300

    EnumDemo2 from chapter 11.

--- java-learning/src/main/java/com/mycompany/java/learning/EnumDemo2.java
+++ java-learning/src/main/java/com/mycompany/java/learning/EnumDemo2.java
@@ -3,5 +3,27 @@ package com.mycompany.java.learning;
 /**
  * Created by greg on 22.05.2016.
  */
-public class EnumDemo2 {
+enum TransportEn1 {
+    CAR, TRUCK, AIRPLANE, TRAIN, BOAT
+}
+class EnumDemo2 {
+    public static void main(String args[])
+    {
+        TransportEn1 tp;
+
+        System.out.println("Here are all Transport constants");
+
+        // воспользоваться методом values()
+        // Получение массива констант типа Transport.
+        TransportEn1 allTransports[] = TransportEn1.values();
+        for(TransportEn1 t : allTransports)
+            System.out.println(t);
+
+        System.out.println();
+
+        // воспользоваться методом valueOf()
+        // Получение константы под названием AIRPLANE.
+        tp = TransportEn1.valueOf("AIRPLANE");
+        System.out.println("tp contains " + tp);
+    }
 }
